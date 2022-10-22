4828c6c44dc601498f8ec4adb578470ed4bc239f
Paulo Jorge <paulojorge2021@gmail.com>
2022-10-22 17:32:20 -0300

    commit

---
+++ metodo.js
@@ -0,0 +1,13 @@
+let pessoa = {
+    nome: "Paulo",
+    idade: 26,
+
+    dizerOla(){
+        console.log("Olá Mundo! Meu nome é " + this.nome)
+    }
+}
+
+
+console.log(pessoa)
+
+pessoa.dizerOla()
